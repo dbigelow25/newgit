43e9b4ad692c2f3a9ff4986e3c5953c9f20e7f67
David Bigelow <david.bigelow@pdgc.com>
2018-02-20 13:03:19 -0800

    fix in database url
    fix on database url

--- scaha/src/com/gbli/connectors/DatabasePool.java
+++ scaha/src/com/gbli/connectors/DatabasePool.java
@@ -42,7 +42,7 @@ public class DatabasePool implements Runnable {
 				for (int i=0; i < m_iCount;i++) {
 					m_vConnections.add(new ScahaDatabase(i,
 							"com.mysql.jdbc.Driver",
-							"jdbc:mysql://	:3306/scaha",
+							"jdbc:mysql://192.241.229.21:3306/scaha",
 							//"jdbc:mysql://192.241.229.21:3306",
 //							"jdbc:mysql://192.241.211.230:3306/scaha",  // original site
 							"scaha", "shiloh24"));
